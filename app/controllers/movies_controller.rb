aa50b465a98881587c9f6e48649820957808510c
David Hayes <davidlynnhayes@att.net>
2015-08-15 04:21:11 -0500

    basic application designed and set up

---
+++ app/controllers/movies_controller.rb
@@ -0,0 +1,34 @@
+class MoviesController < ApplicationController
+
+  # get
+  def index
+    # render a list ... give a form to check off a movie
+  end
+
+  # get
+  def new
+    # displays a form to create a new movie queue item
+  end
+
+  # post
+  def create
+    # handle the form submission and add to database
+  end
+
+  # post
+  def update
+    # lets a user check off a movie from the queue
+  end
+
+  # patch
+  def delete
+    # lets a user delete a movie if they have not checked it off
+    # else...they can't, they've already marked it watched.
+  end
+
+  # get
+  def confirmation
+    # catch-all confirmation
+
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  # set landing page (in place of welcome to Rails)
+  root 'movies#index'
+
+  # destinations
+  get 'movies' => 'movies#index'
+  get 'movies/new' =>'movies#new'
+  post 'movies/create' => 'movies#create'
+  post 'movies/watched' => 'movies#update'
+  patch 'movies/delete' => 'movies#delete'
+  get 'movies/confirmation' => 'movies#confirmation'
+
+  # sessions - login/logout
+  get 'users/login' => 'sessions#login'
+  post 'users/login_confirmation' => 'sessions#confirmation'
+  get 'users/logout' => 'sessions#logout'
+
+  # http verb (get/post/patch) - 'resource/route' => mapped to a controller#method
+  get 'users/register' => 'users#signup'
+  post 'users/confirmation' => 'users#confirmation'
+
+end
