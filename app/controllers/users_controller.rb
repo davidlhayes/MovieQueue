c448384f69b91249354c794f3766cd5d72d47cd9
David Hayes <davidlynnhayes@att.net>
2015-08-15 15:00:01 -0500

    sessions work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,16 +8,30 @@ class UsersController < ApplicationController
 
   # post
   def confirmation
-    puts '---------movies#confirmation----------'
-    puts user_params
-    puts '---------movies#confirmation----------'
+    # puts '---------movies#confirmation----------'
+    # puts user_params
+    # puts '---------movies#confirmation----------'
     # post request that handles registration/says thanks
+
+    @user = User.new(user_params)
+    if @user.save
+      # thanks! success!
+      session[:user_id] = @user.id
+
+      # Movies.where(:user_id => session[:user_id].to_i)
+
+    else
+      # error message
+      @message = 'User account exists or passwords do not match.'
+      render 'users/signup'
+    end
+
   end
 
 private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:username, :email, :password, :password_confirmation)
     end
 
 end
